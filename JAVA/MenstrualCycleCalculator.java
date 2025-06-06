20f107fd707988ae053e689eb4e6110d884717a0
GiftedGiftB <asekhamegift@gmail.com>
2025-06-06 23:17:04 +0100

    app that calculates menstral cycle

--- JAVA/MenstrualCycleCalculator.java
+++ JAVA/MenstrualCycleCalculator.java
@@ -3,8 +3,17 @@ public class MenstrualCycleCalculator{
 	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
 
+	System.out.print("""
+
+		Menstral cycle App
+
+""");
+
 	int userInput1;
 	int userInput2;
+	int lengthCycle;
+
+	int Temp = 0;
 	
 	System.out.print("Enter current date at which your menstral cycle started: ");
 	userInput1 = input.nextInt();
@@ -12,9 +21,21 @@ public class MenstrualCycleCalculator{
 	System.out.print("Enter last date of when you started your menstral cycle: ");	
 	userInput2 = input.nextInt();
 
-	int cycle = userInput1 - userInput2;
+	if(userInput1 > userInput2){
+	userInput1 = userInput2;
+	}
+	else if (userInput1 < userInput2){
+	Temp = userInput1;
+	userInput1 = userInput2;
+	userInput2 = Temp;
+	}
 
-	System.out.print("The length of your cycle is: " + cycle);
+	lengthCycle = userInput1 - userInput2;
+
+
+	System.out.print("The length of your cycle is: " + lengthCycle);
+	//System.out.println("Your ovulation day is: " + );
+
 
 }
-} 
+}
