7fd753b0a14e1c3c5a7acfbc8a15c78c39fbaf20
GiftedGiftB <asekhamegift@gmail.com>
2025-06-06 21:21:08 +0100

    creating an app to calculate menstral cycle period

--- JAVA/MenstrualCycleCalculator.java
+++ JAVA/MenstrualCycleCalculator.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class MenstrualCycleCalculator{
 	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	int userInput1;
+	int userInput2;
+	System.out.print("Enter length of cycle: ");
+	int userInput1 = input.nextInt();
+
+	System.out.print("Enter last time you had your period: ");
+	int userInput2 = input.nextInt();
+
 
 	
-
 }
 }
