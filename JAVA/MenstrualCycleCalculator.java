441a03519020c5d7c9393a82f442444ed805fec2
GiftedGiftB <asekhamegift@gmail.com>
2025-06-07 00:01:01 +0100

    menstrual cycle app

--- JAVA/MenstrualCycleCalculator.java
+++ JAVA/MenstrualCycleCalculator.java
@@ -11,8 +11,10 @@ public class MenstrualCycleCalculator{
 
 	int userInput1;
 	int userInput2;
-	int lengthCycle;
+	int userInput3;
 
+	int lengthCycle;
+	
 	int Temp = 0;
 	
 	System.out.print("Enter current date at which your menstral cycle started: ");
@@ -21,21 +23,27 @@ public class MenstrualCycleCalculator{
 	System.out.print("Enter last date of when you started your menstral cycle: ");	
 	userInput2 = input.nextInt();
 
+	System.out.print("Enter date you stoped menstrating: ");
+	userInput3 = input.nextInt();
+
 	if(userInput1 > userInput2){
-	userInput1 = userInput2;
-	}
+		userInput1 = userInput2;
+		}
 	else if (userInput1 < userInput2){
-	Temp = userInput1;
-	userInput1 = userInput2;
-	userInput2 = Temp;
-	}
+		Temp = userInput1;
+		userInput1 = userInput2;
+		userInput2 = Temp;
+		}
 
-	lengthCycle = userInput1 - userInput2;
+		lengthCycle = userInput1 - userInput2;
+	
+		int ovulationDay = lengthCycle - 14;
 
+		//safePeriod = 
 
-	System.out.print("The length of your cycle is: " + lengthCycle);
-	//System.out.println("Your ovulation day is: " + );
-
+			System.out.println("The length of your cycle is: " + lengthCycle);
+			System.out.println("Your ovulation day is in: " + ovulationDay + " days");
+			//System.out.print("Your safe period is: " + safePeriod " days after you've stoped menstrating");
 
 }
 }
