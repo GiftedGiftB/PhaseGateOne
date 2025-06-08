e7628674bfd857d60b2b6767262a5b0ca70da2c1
GiftedGiftB <asekhamegift@gmail.com>
2025-06-08 17:15:30 +0100

    Menstrual Cycle Calculator

---
+++ JAVASCRIPT/MenstrualCycleCalculator.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+console.log("      Menstrual Cycle App  ");
+
+let temp;
+
+let lastCycleStartDate = Number(prompt("Enter last cycle start date (in days, e.g., 1-31): "));
+
+let currentCycleStartDate = Number(prompt("Enter current cycle start date (in days, e.g., 1-31): "));
+
