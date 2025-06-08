a2357477d1f3fbbd3bbf50a5dee6ef56b9a4372b
GiftedGiftB <asekhamegift@gmail.com>
2025-06-08 17:18:59 +0100

    Menstrual Cycle Calculator

--- JAVASCRIPT/MenstrualCycleCalculator.js
+++ JAVASCRIPT/MenstrualCycleCalculator.js
@@ -8,3 +8,14 @@ let lastCycleStartDate = Number(prompt("Enter last cycle start date (in days, e.
 
 let currentCycleStartDate = Number(prompt("Enter current cycle start date (in days, e.g., 1-31): "));
 
+if (lastCycleStartDate > currentCycleStartDate) {
+  temp = lastCycleStartDate;
+  lastCycleStartDate = currentCycleStartDate;
+  currentCycleStartDate = temp;
+}
+
+let cycleLength = currentCycleStartDate - lastCycleStartDate;
+
+
+console.log("The length of your cycle is: " + cycleLength);
+
