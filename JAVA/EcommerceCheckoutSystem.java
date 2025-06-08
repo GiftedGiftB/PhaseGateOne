3570e536a4dce8bcbc0a7ef0bf7bd32114a25077
GiftedGiftB <asekhamegift@gmail.com>
2025-06-08 01:43:29 +0100

    Ecommerce checkout system

--- JAVA/EcommerceCheckoutSystem.java
+++ JAVA/EcommerceCheckoutSystem.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 public class EcommerceCheckoutSystem{
 	public static void main(String[] args){
-	
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("What is the customer's name: ");
+	String customerName = input.next();
+
+	System.out.print("What did the user buy: ");
+	String userItems = input.next();
+
+	System.out.print("How many pieces:");
+	int pieces = input.nextInt();
+
+	System.out.print("How much per unit: ");
+	int perUnits = input.nextInt();
+
+	System.out.print("Add more items?: ");
+	int addItems = input.nextInt();
+
+
 
 
 }
