f2a4ddfc0e3919156f8f9319ac497eb4fab52dd0
GiftedGiftB <asekhamegift@gmail.com>
2025-06-09 10:54:19 +0100

    menstrual cycle calculator app

--- PYTHON/menstrualcyclecalculator.py
+++ PYTHON/menstrualcyclecalculator.py
@@ -19,3 +19,10 @@ ovulation_day = last_cycle_start_date + cycle_length // 2
 print(f"The length of your cycle is: {cycle_length}")
 
 print(f"Your ovulation day is approximately on day: {ovulation_day}")
+
+safe_period_start = ovulation_day + 2
+safe_period_end = last_cycle_start_date + cycle_length
+
+print(f"Your safe period is approximately from day {safe_period_start} to day {safe_period_end}")
+
+
