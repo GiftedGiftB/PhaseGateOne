95aba152d85a728f25bb2e2609f5b5271cc838be
GiftedGiftB <asekhamegift@gmail.com>
2025-06-09 10:52:34 +0100

    menstrual cycle calculator app

--- PYTHON/menstrualcyclecalculator.py
+++ PYTHON/menstrualcyclecalculator.py
@@ -7,4 +7,15 @@ last_cycle_start_date = int(input("Enter last cycle start date (in days, e.g., 1
 
 current_cycle_start_date = int(input("Enter current cycle start date (in days, e.g., 1-31): "))
 
+if last_cycle_start_date > current_cycle_start_date:
+    temp = last_cycle_start_date
+    last_cycle_start_date = current_cycle_start_date
+    current_cycle_start_date = temp
 
+cycle_length = current_cycle_start_date - last_cycle_start_date
+
+ovulation_day = last_cycle_start_date + cycle_length // 2
+
+print(f"The length of your cycle is: {cycle_length}")
+
+print(f"Your ovulation day is approximately on day: {ovulation_day}")
