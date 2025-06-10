3867fd7924b5844994593cd64db21ee5da884951
GiftedGiftB <asekhamegift@gmail.com>
2025-06-10 20:54:29 +0100

    MBTI Personality Test

--- JAVA/MbtiPersonalityTest.java
+++ JAVA/MbtiPersonalityTest.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 public class MbtiPersonalityTest{
 	public static void main(String[] args){
-
+	Scanner input = new Scanner(System.in);
 
 	System.out.print("""
 
@@ -9,7 +10,16 @@ public class MbtiPersonalityTest{
 
 
 
+	System.out.println("When making decisions, what do you prioritize?");
 
+        System.out.println("A. Logical analysis and objective criteria.");
+        System.out.println("B. Personal values and the impact on others.");
+
+        System.out.print("Enter (A or B): ");
+
+	String choice1 = input.next().toUpperCase();
+	if (choice3.equals("A")) {
+        }
 
 
 
