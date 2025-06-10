08375ec24c4c447037abfc57858a98ffe8b8d426
GiftedGiftB <asekhamegift@gmail.com>
2025-06-10 10:47:30 +0100

    Semicolon Ecommerce Checkout System

--- JAVASCRIPT/EcommerceCheckoutSystem.js
+++ JAVASCRIPT/EcommerceCheckoutSystem.js
@@ -1,5 +1,23 @@
+const prompt = require('prompt-sync')();
+
 console.log()
 
 console.log("		Semicolon Ecommerce Checkout System")
 
-	
+
+
+    let customerName = prompt("What is the customer's name: ");
+
+    let userItems = prompt("What did the user buy: ");
+
+    let pieces = parseInt(prompt("How many pieces: "));
+
+    let perUnits = parseFloat(prompt("How much per unit: "));
+
+    let total = pieces * perUnits;
+
+    let valueAddedTax = total * 0.075;
+
+    let totalWithVat = total + valueAddedTax;
+
+   
