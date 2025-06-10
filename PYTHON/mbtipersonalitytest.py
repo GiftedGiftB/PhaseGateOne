d84c33bff28fc3149b937de0294f216870dbfc16
GiftedGiftB <asekhamegift@gmail.com>
2025-06-10 21:08:15 +0100

    MBTI Personality Test

---
+++ PYTHON/mbtipersonalitytest.py
@@ -0,0 +1,8 @@
+print("	MBTI Personality Test ");
+
+print("When making decisions, what do you prioritize?");
+
+print("A. Logical analysis and objective criteria.");
+print("B. Personal values and the impact on others.");
+
+print("Enter (A or B): ");
