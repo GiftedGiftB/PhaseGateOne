9582c37debe3fe471f130cc34ad19c391c3f73b9
GiftedGiftB <asekhamegift@gmail.com>
2025-06-10 21:02:03 +0100

    MBTI Personality Test

---
+++ JAVASCRIPT/MbtiPersonalityTest.js
@@ -0,0 +1,10 @@
+console.log("	MBTI Personality Test ");
+
+console.log("When making decisions, what do you prioritize?");
+
+console.log("A. Logical analysis and objective criteria.");
+console.log("B. Personal values and the impact on others.");
+
+console.log("Enter (A or B): ");
+
+	
