839f3c09bc6554778c89255bec2f5784c132ee5d
GiftedGiftB <asekhamegift@gmail.com>
2025-06-10 13:19:07 +0100

    Semicolon Ecommerce Checkout System

--- JAVASCRIPT/EcommerceCheckoutSystem.js
+++ JAVASCRIPT/EcommerceCheckoutSystem.js
@@ -5,19 +5,48 @@ console.log()
 console.log("		Semicolon Ecommerce Checkout System")
 
 
+let isRunning = true;
 
-    let customerName = prompt("What is the customer's name: ");
+let customerName = prompt("What is the customer's name: ");
 
-    let userItems = prompt("What did the user buy: ");
+//while (isRunning) {
 
-    let pieces = parseInt(prompt("How many pieces: "));
+	let userItems = prompt("What did the user buy: ");
 
-    let perUnits = parseFloat(prompt("How much per unit: "));
+	let pieces = parseInt(prompt("How many pieces: "));
 
-    let total = pieces * perUnits;
+	let perUnits = parseFloat(prompt("How much per unit: "));
 
-    let valueAddedTax = total * 0.075;
 
-    let totalWithVat = total + valueAddedTax;
+	let total = pieces * perUnits;
 
-   
+	let valueAddedTax = total * 0.075;
+
+	let totalWithVat = total + valueAddedTax;
+
+
+
+	console.log("Customer Name: " + customerName);	
+
+	console.log("Item: $" + userItems);
+
+	console.log("Quantity: $" + pieces);
+
+	console.log("Price: $" + perUnits);
+
+	console.log("Total: $" + total);
+
+	console.log("The Value-added Tax (7 .5%) is: $" + valueAddedTax);
+
+	console.log("Total with VAT: $" + totalWithVat);
+
+	let addItems = prompt("Add more items? (1 for yes and 0 for no): ");
+
+
+ /*   if (addItems == 0){
+        isRunning = false;
+    }
+} */
+
+	console.log("THANK YOU FOR YOUR PATRONAGE");
+
