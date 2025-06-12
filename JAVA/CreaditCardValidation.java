36609080ee97fe81c54ade44b0deb2d1cd5978ca
GiftedGiftB <asekhamegift@gmail.com>
2025-06-12 02:05:45 +0100

    creadit card validation

--- JAVA/CreaditCardValidation.java
+++ JAVA/CreaditCardValidation.java
@@ -9,14 +9,30 @@ public class CreaditCardValidation{
 	userInput = input.next();
 
 	int length = userInput.length();
-	
 	System.out.println(length);
 
 	char firstNumber = userInput.charAt(0);
 	char secondNumber = userInput.charAt(1);
 	System.out.println(firstNumber);
 	
-		
+	if (length > 12 && length < 17) {
+            System.out.println("Valid");
+            if (firstNumber == '4') {
+                System.out.println("Visa Card");
+            } else if (firstNumber == '5') {
+                System.out.println("Master Card");
+            } else if (firstNumber == '3' && secondNumber == '7'){
+                System.out.println("American Express Card");
+            } else if (firstNumber == '6') {
+                System.out.println("Discover Card");
+            } else {
+                System.out.println("Not recognise");
+            }
+        } else {
+            System.out.println("Invalid");
+        }
+
+	
 }
 
 }
