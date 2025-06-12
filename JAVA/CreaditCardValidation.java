02873429ed943589f601a2c4daf2a35d6863309f
GiftedGiftB <asekhamegift@gmail.com>
2025-06-12 02:30:20 +0100

    Creadit card validation

--- JAVA/CreaditCardValidation.java
+++ JAVA/CreaditCardValidation.java
@@ -9,28 +9,45 @@ public class CreaditCardValidation{
 	userInput = input.next();
 
 	int length = userInput.length();
-	System.out.println(length);
+	//System.out.println(length);
 
 	char firstNumber = userInput.charAt(0);
 	char secondNumber = userInput.charAt(1);
-	System.out.println(firstNumber);
+	//System.out.println(firstNumber);
 	
-	if (length > 12 && length < 17) {
-            System.out.println("Valid");
-            if (firstNumber == '4') {
-                System.out.println("Visa Card");
-            } else if (firstNumber == '5') {
-                System.out.println("Master Card");
-            } else if (firstNumber == '3' && secondNumber == '7'){
-                System.out.println("American Express Card");
-            } else if (firstNumber == '6') {
-                System.out.println("Discover Card");
-            } else {
-                System.out.println("Not recognise");
-            }
-        } else {
-            System.out.println("Invalid");
-        }
+	if(length > 12 && length < 17){
+		if (firstNumber == '4'){
+		System.out.println("*******************************************");
+		System.out.println("**Creadit Card Type: Visa Card");
+		}
+		else if (firstNumber == '5'){
+		System.out.println("*******************************************");
+		System.out.println("**Creadit Card Type: Master Card");
+		}
+		else if (firstNumber == '3' && secondNumber == '7'){
+		System.out.println("*******************************************");
+                System.out.println("**Creadit Card Type: American Express Card");
+		}
+		else if (firstNumber == '6'){
+		System.out.println("*******************************************");
+                System.out.println("**Creadit Card Type: Discover Card");
+		}
+		else{
+		System.out.println("*******************************************");
+		System.out.println("**Creadit Card Type: Not recognise");
+		}
+	System.out.println("**Creadit card Number: " + userInput);
+	System.out.println("**Creadit Card digit length: " + length);
+
+	System.out.println("**Cradit card validity status: Valid");
+	}
+	else{
+	System.out.println("**Cradit card validity status: Invalid");
+	}
+	System.out.println("*******************************************");
+
+	
+
 
 	
 }
