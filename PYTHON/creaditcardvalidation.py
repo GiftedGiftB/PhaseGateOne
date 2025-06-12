756e610b28ff85055cd42ed950646ce6f3de3dd0
GiftedGiftB <asekhamegift@gmail.com>
2025-06-12 13:26:18 +0100

    Creadit card validation

--- PYTHON/creaditcardvalidation.py
+++ PYTHON/creaditcardvalidation.py
@@ -1,1 +1,13 @@
 user_input = input("Enter credit card Number: ")
+
+length = len(user_input)
+print(length)
+
+first_number = user_input[0]
+print(first_number)
+
+if length > 12 and length < 17:
+	print("Valid")
+
+else:
+	print("Invalid")
